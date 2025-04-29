a81eda85563b0b1a604768f2f8e5fae62255f786
Ganesh Arwat <ganesharwat123@gmail.com>
2025-04-29 09:31:35 +0530

    env variable added

--- Frontend/src/providers/SocketProvider.jsx
+++ Frontend/src/providers/SocketProvider.jsx
@@ -2,10 +2,11 @@ import { createContext, useContext, useMemo } from "react";
 import { io } from "socket.io-client";
 
 const SocketContext = createContext(null);
+const backendUrl = import.meta.env.VITE_SOCKET_SERVER;
 
 export const SocketProvider = ({ children }) => {
   const serverUrl = "http://localhost:5000"
-  const socket = useMemo(() => io(serverUrl), []);
+  const socket = useMemo(() => io(backendUrl || serverUrl), []);
   return (
     <SocketContext.Provider value={{ socket }}>
       {children}
