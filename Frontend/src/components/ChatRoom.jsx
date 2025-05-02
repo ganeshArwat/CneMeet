230690f660d14548cfd2adef08bc06fe48dfc7ad
Ganesh Arwat <ganesharwat123@gmail.com>
2025-05-02 13:55:35 +0530

    chat room responsive issue resolved

--- Frontend/src/components/ChatRoom.jsx
+++ Frontend/src/components/ChatRoom.jsx
@@ -20,16 +20,18 @@ function ChatRoom({ sendMessage, setMessages, messages }) {
     };
     return (
         <>
-            <div className="bg-gray-800 rounded-lg p-4 flex flex-col h-full">
+            <div className="md:h-[83vh] bg-gray-800 flex flex-col p-4 rounded-lg">
                 <h1 className="text-center text-2xl font-semibold text-neutral-300 mb-4">Chat Room</h1>
-                <div className="flex-grow bg-gray-900 p-4 overflow-y-auto rounded-lg mb-4 max-h-[320px]">
-                    {messages.map((message) => (
+
+                {/* Message Container */}
+                <div className="flex-1 bg-gray-900 p-4 overflow-y-auto rounded-lg mb-4">
+                    {messages.map((message) =>
                         message.type === "sent" ? (
                             <SentMessage key={message.id} message={message.text} timestamp={message.timestamp} />
                         ) : (
                             <ReceivedMessage key={message.id} message={message.text} timestamp={message.timestamp} />
                         )
-                    ))}
+                    )}
                 </div>
 
                 {/* Chat Input Section */}
@@ -41,13 +43,12 @@ function ChatRoom({ sendMessage, setMessages, messages }) {
                         value={inputValue}
                         onChange={(e) => setInputValue(e.target.value)}
                         onKeyUp={(e) => {
-                            // Check if the Enter key is pressed
-                            if (e.key === "Enter") {
-                                handleSendMessage();
-                            }
+                            if (e.key === "Enter") handleSendMessage();
                         }}
                     />
-                    <button className="p-2 bg-blue-600 text-white rounded-lg" onClick={handleSendMessage}><IoMdSend /></button>
+                    <button className="p-2 bg-blue-600 text-white rounded-lg" onClick={handleSendMessage}>
+                        <IoMdSend />
+                    </button>
                 </div>
             </div>
         </>
